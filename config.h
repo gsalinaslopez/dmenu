c224ff5ef5cfcbbe7d8b0ec065ee3222f9774afa
Giovanni Salinas <gsalinaslopez@gmail.com>
2021-05-25 10:30:45 +0800

    color profiles

--- config.h
+++ config.h
@@ -12,7 +12,8 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#ffffff", "#000000" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeSel] = { "#ffffff", "#000000" },
+	/* [SchemeSel] = { "#eeeeee", "#005577" }, */
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
